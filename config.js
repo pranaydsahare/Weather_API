76a44bd3445d67622c60e73b9c93dafdaa13e002
Pranay Sahare <pranaysahare38@gmail.com>
2024-01-24 22:18:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "8acf33c36aa922e2627d72f75373dc79"
+        
+    }
+}
+
+module.exports = constants;
